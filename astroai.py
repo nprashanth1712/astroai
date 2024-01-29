d0a1fcec1271b1cee39cc0f45aeb1b976e53d14c
nprashanth1712 <96611409+nprashanth1712@users.noreply.github.com>
2024-01-29 15:38:01 +0530

    Add files via upload

---
+++ astroai.py
@@ -0,0 +1,85 @@
+import openai
+import streamlit as st
+import time
+
+#from openai import OpenAI
+client = openai()
+
+#empty_thread = client.beta.threads.create()
+#print(empty_thread)
+
+assistant_id = "asst_kcijOKWplEtKqVuk1QMgSpX0"
+
+#client = openai
+
+if "start_chat" not in st.session_state:
+    st.session_state.start_chat = False
+if "thread_id" not in st.session_state:
+    st.session_state.thread_id = None
+
+st.set_page_config(page_title="CatGPT", page_icon=":speech_balloon:")
+
+openai.api_key = "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"
+
+if st.sidebar.button("Start Chat"):
+    st.session_state.start_chat = True
+    thread = client.beta.threads.create()
+    st.session_state.thread_id = thread.id
+
+st.title("ASTRO AI chat bot")
+st.write("Hi I'm a astro bot")
+
+if st.button("Exit Chat"):
+    st.session_state.messages = []  # Clear the chat history
+    st.session_state.start_chat = False  # Reset the chat state
+    st.session_state.thread_id = None
+
+if st.session_state.start_chat:
+    if "openai_model" not in st.session_state:
+        st.session_state.openai_model = "gpt-4-1106-preview"
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+    
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    if prompt := st.chat_input(""):
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.markdown(prompt)
+
+        client.beta.threads.messages.create(
+                thread_id=st.session_state.thread_id,
+                role="user",
+                content=prompt
+            )
+        
+        run = client.beta.threads.runs.create(
+            thread_id=st.session_state.thread_id,
+            assistant_id=assistant_id,
+            instructions="Your are a hindu vedic astrology  expert with the knowledge of vedic astrology you will be given a doc which is book called Brihat Parashara hora shastra and you have to answer all question according to this book if know the answer just say i dont have relevant knowledge  and you only answer to astrological related question and any other question not related to astrology should be answered with  i dont have relevant knowledge"
+        )
+
+        while run.status != 'completed':
+            time.sleep(1)
+            run = client.beta.threads.runs.retrieve(
+                thread_id=st.session_state.thread_id,
+                run_id=run.id
+            )
+        messages = client.beta.threads.messages.list(
+            thread_id=st.session_state.thread_id
+        )
+
+        # Process and display assistant messages
+        assistant_messages_for_run = [
+            message for message in messages 
+            if message.run_id == run.id and message.role == "assistant"
+        ]
+        for message in assistant_messages_for_run:
+            st.session_state.messages.append({"role": "assistant", "content": message.content[0].text.value})
+            with st.chat_message("assistant"):
+                st.markdown(message.content[0].text.value)
+
+else:
+    st.write("Click 'Start Chat' to begin.")
